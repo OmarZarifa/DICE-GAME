48accd61c25eaa17859132ea597f0eec58b8962e
Nirmin <nirmia96@zedat.fu-berlin.de>
2023-03-04 18:44:29 +0100

    test_highScore.py is done

--- app/highScore.py
+++ app/highScore.py
@@ -1,21 +1,19 @@
 class HighScore:
-    
-    
-    
+
     def __init__(self):
         self.players_name = []
         self.players_score = []
-         
+
+
     def add_score(self, score):
         self.players_score.append(score)
+        
         
     def add_name(self, name):
         self.players_name.append(name)
     
     
     def display(self):
-        if self.players_name != None and self.players_score != None:
-
             # Print the table headers
             print("{:<10} {:<10}".format("Player", "Score"))
             print("=" * 20)
@@ -23,5 +21,3 @@ class HighScore:
             # Print the table rows
             for i in range(len(self.players_name)):
                 print("{:<10} {:<10}".format(self.players_name[i], self.players_score[i]))
-        else:
-            print("there is no scores")
--- tests/test_highScore.py
+++ tests/test_highScore.py
@@ -1,21 +1,47 @@
 
 import unittest
 from app.highScore import HighScore
+import io
 
 class TestHighScore(unittest.TestCase):
 
     def setUp(self):
         self.highScore = HighScore()
 
+
     def test_add_score(self):
         # Ensure score is added to list of scores
         self.highScore.add_score(10)
         self.assertEqual(self.highScore.players_score, [10])
 
+
     def test_add_name(self):
         # Ensure name is added to list of names
         self.highScore.add_name("John")
         self.assertEqual(self.highScore.players_name, ["John"])
+
+
+    def test_display_with_score(self):
+        # Ensure name and scores is added
+        self.highScore.add_name("Alice")
+        self.highScore.add_score(100)
+        expected_output = "Player     Score     \n" \
+                          "====================\n" \
+                          "Alice      100"
+        
+        with unittest.mock.patch("sys.stdout", new=io.StringIO()) as fake_output:
+            self.highScore.display()
+            self.assertEqual(fake_output.getvalue().strip(), expected_output)
+            
+
+    def test_display_without_score(self):
+        
+        expected_output = "Player     Score     \n" \
+                          "===================="
+                          
+        with unittest.mock.patch("sys.stdout", new=io.StringIO()) as fake_output:
+            self.highScore.display()
+            self.assertEqual(fake_output.getvalue().strip(), expected_output)
 
 if __name__ == '__main__':
     unittest.main()
