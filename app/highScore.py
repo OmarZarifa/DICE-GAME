1a278fa57b3f3e8dd31514b0b56c9ac4a47f3bbe
Nirmin <nirmia96@zedat.fu-berlin.de>
2023-03-06 13:51:58 +0100

    highscore docstrings

--- app/highScore.py
+++ app/highScore.py
@@ -1,15 +1,43 @@
+
+"""
+The HighScore class is for keeping track of high scores for a game.
+
+The class stores player names and scores and can display a high score table.    
+"""
+
 class HighScore:
+    """
+    A class for keeping track of high scores for a game.
+    """
     def __init__(self):
+        """
+        Initializes an empty list for player names and an empty list for player scores.
+        """
         self.players_name = []
         self.players_score = []
 
     def add_score(self, score):
+        """
+        Adds a player's score to the list of scores.
+
+        Args:
+            score (int): The score to add to the list.
+        """
         self.players_score.append(score)
 
     def add_name(self, name):
+        """
+        Adds a player's name to the list of names.
+
+        Args:
+            name (str): The name to add to the list.
+        """
         self.players_name.append(name)
 
     def display(self):
+        """
+        Displays the high score table with player names and scores.
+        """
         # Print the table headers
         print("{:<10} {:<10}".format("Player", "Score"))
         print("=" * 20)
